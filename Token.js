55d0b150c9a5e473bf32767dc55b67a1075065ed
Putra-4321 <matisaya41@gmail.com>
2025-08-18 20:56:38 +0700

    Tambah token via panel

--- Token.js
+++ Token.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "7861139269:AAGqizWuvnfB3qpqd35Xj0wFEeeoshcIMLs",
-    "7487580586:AAHllyZqAvX9JlfzRNJfMKor8i8oqBNBiMQ"
+    "7487580586:AAHllyZqAvX9JlfzRNJfMKor8i8oqBNBiMQ",
+    "8465027407:AAFCnvzgDuPFoZLliaSXFy0a2Kh6QJcYFHI"
   ]
 }
